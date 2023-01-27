38d29edcec395dee2933a1f5467854f00e320657
Annie Huang <annie.hn@gmail.com>
2023-01-27 17:52:34 +1100

    Make adjustment.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const multiStepForm = document.querySelector('[data-multi-step]');
-const formSteps = [...document.querySelectorAll('[data-step]')];
+const formSteps = [...multiStepForm.querySelectorAll('[data-step]')];
 
-let currentStep = parseInt(
-  formSteps.findIndex((step) => step.classList.contains('active'))
-);
+let currentStep = formSteps.findIndex((step) => {
+  return step.classList.contains('active');
+});
 
 // console.log(currentStep);
 if (currentStep < 0) {
@@ -17,7 +17,7 @@ multiStepForm.addEventListener('click', (e) => {
   if (e.target.matches('[data-next]')) {
     incrementor = 1;
   } else if (e.target.matches('[data-previous]')) {
-    incrementor = 1;
+    incrementor = -1;
   }
 
   if (incrementor == null) return;
@@ -29,11 +29,18 @@ multiStepForm.addEventListener('click', (e) => {
 
   // When a field failed validation, you need to hover in the field to be able to see the default error message in chrome
   // Need to do it both way, not ideal. But if you only do it for next, not previous, then if there is fail validation in the current page, then user click previous and then next, they will never be able to go back to current page.
-  const allValid = inputs.every((input) => input.checkValidity());
+  const allValid = inputs.every((input) => input.reportValidity());
   if (allValid) {
     currentStep += incrementor;
     showCurrentStep();
   }
+});
+
+formSteps.forEach((step) => {
+  step.addEventListener('animationend', (e) => {
+    formSteps[currentStep].classList.remove('hide');
+    e.target.classList.toggle('hide', !e.target.classList.contains('active'));
+  });
 });
 
 function showCurrentStep() {
